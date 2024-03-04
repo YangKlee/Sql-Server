853a49447104ec7f3fd964fc9d5339e20ca8f6d8
YangKlee <khanhduong18072005@gmail.com>
2024-03-04 21:01:58 +0700

    update

--- baithuchanh2.sql
+++ baithuchanh2.sql
@@ -19,8 +19,10 @@ where NuocSX = N'Nhật Bản' or NuocSX = N'Nhật' or NuocSX = 'Japan'
 select KhachHang.* from KhachHang, CTHD, HoaDon, SanPham
 where SanPham.TenSP = N'Sữa Dango' and SanPham.MaSP = CTHD.MaSP and HoaDon.SoHD = CTHD.SoHD and HoaDon.MaKH = KhachHang.MaKH
 -- don hang so HD26020307 do ai ai dat, nhan vien nao lap, tri gia bao nhieu
-select HoaDon.SoHD ,KhachHang.HoTen, NhanVien.HoTen, SanPham.TenSP,HoaDon.TriGia from KhachHang, NhanVien, HoaDon, CTHD, SanPham
-where HoaDon.SoHD = 'HD26020307' and HoaDon.MaNV = NhanVien.MaNV and HoaDon.MaKH = KhachHang.MaKH and CTHD.SoHD = HoaDon.SoHD and CTHD.MaSP = SanPham.MaSP
+select HoaDon.SoHD ,KhachHang.HoTen, NhanVien.HoTen as 'Nhan vien lap', SanPham.TenSP,HoaDon.TriGia 
+from KhachHang, NhanVien, HoaDon, CTHD, SanPham
+where HoaDon.SoHD = 'HD26020307' and HoaDon.MaNV = NhanVien.MaNV 
+and HoaDon.MaKH = KhachHang.MaKH and CTHD.SoHD = HoaDon.SoHD and CTHD.MaSP = SanPham.MaSP
 
 
 
@@ -30,18 +32,14 @@ add GioiTinh bit null
 --xoa cot email nhanvien
 alter table NhanVien
 drop column Email
--- them san pham may tinh xanh tay vao cot san pham
-insert into SanPham
-values
-('SP24225049', N'Máy tính Apple', N'Mỹ', 'Mỹ', 15000000, 350)
--- tang gia san pham may tinh len 10%
+
 update SanPham
 set Gia += Gia * 0.1
 where TenSP like N'%Máy tính%'
 
 -- hien thi danh sach khach hang chua co so dien thoai
 select * from KhachHang
-where SoDt = NULL
+where SoDt is null
 
 -- san pham chua ban duoc
 select distinct  SanPham.MaSP, SanPham.TenSP from SanPham, CTHD, HoaDon
